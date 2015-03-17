c80537f35690b6711d566e46b85b9ceb7b63171b
Christopher Weir <christopher_weir@me.com>
2015-03-17 17:26:13 +0000

    Refactored the code

--- angular-matchheight.js
+++ angular-matchheight.js
@@ -9,27 +9,44 @@ angular.module('angular-matchheight', [])
         function () {
         return {
             restrict: 'A',
+            scope: {},
             link:   function ( scope, elm, attrs ) {
-                if( scope.ilnMatchHeightArr === undefined ){
-                    scope.ilnMatchHeightArr = [elm[0].offsetHeight];
-                }else{
-                    scope.ilnMatchHeightArr.push(elm[0].offsetHeight);
-                }
 
+                var i;
+                var heightsArray = [];
+                var largest = 0;
+
+                /**
+                 * Return the largest number in the array
+                 * @name largetsNo
+                 * @param  {Array} array - The array of heights
+                 * @return {Number} the largest number
+                 */
                 var largetsNo = function( array ){
                     return Math.max.apply( Math, array );
                 };
 
-                scope.$watch(scope.ilnMatchHeightArr, function(){
-                    elm.css('height', largetsNo(scope.ilnMatchHeightArr)+'px');
-                });
+                // when the element has finished loading
+                elm.ready(function(){
 
-                scope.$on(
-                    '$destroy',
-                    function() {
-                        scope.ilnMatchHeightArr = [];
+                    // loop through all the children and get their heights
+                    for ( i = 0; i < elm[0].childNodes.length; i++) {
+
+                        // add the heights to the array if not undefined
+                        // e.g a comment will be picked up
+                        if( elm[0].childNodes[i].offsetHeight ){
+                            heightsArray.push( elm[0].childNodes[i].offsetHeight );
+                        }
                     }
-                );
+
+                    // get the largest height
+                    largest = largetsNo( heightsArray );
+
+                    // add the height to the children
+                    for ( i = 0; i < elm[0].childNodes.length; i++) {
+                        elm.children().eq( i ).css( 'height', largest + 'px' );
+                    }
+                });
             }
         };
     }]);
